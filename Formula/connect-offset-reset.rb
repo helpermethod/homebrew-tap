d165bf2c4653f67b6fb5b98e1af5686663407893
jreleaserbot <jreleaser@kordamp.org>
2021-08-11 20:13:58 +0000

    connect-offset-reset v0.5.0

--- Formula/connect-offset-reset.rb
+++ Formula/connect-offset-reset.rb
@@ -1,9 +1,9 @@
 class ConnectOffsetReset < Formula
   desc "A commandline tool for resetting Kafka Connect source connector offsets."
   homepage "https://github.com/helpermethod/connect-offset-reset"
-  version "0.4.0"
-  url "https://github.com/helpermethod/connect-offset-reset/releases/download/v0.4.0/connect-offset-reset-0.4.0-osx-x86_64.zip"
-  sha256 "6bc4020e5fe0cf883f3dc731b097fb44a7f3adf15747d14e43c7557bdb8adabf"
+  version "0.5.0"
+  url "https://github.com/helpermethod/connect-offset-reset/releases/download/v0.5.0/connect-offset-reset-0.5.0-osx-x86_64.zip"
+  sha256 "0c5cf90f0ef7a4222952dd962b81bff94f1477f08623dce7da6c0007814210fe"
   license "Apache-2"
 
   bottle :unneeded
@@ -16,6 +16,6 @@ class ConnectOffsetReset < Formula
 
   test do
     output = shell_output("#{bin}/connect-offset-reset --version")
-    assert_match "0.4.0", output
+    assert_match "0.5.0", output
   end
 end
