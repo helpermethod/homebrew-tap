613cafea88a97dfb778190d8deb6ea4418c24274
jreleaserbot <jreleaser@kordamp.org>
2022-01-06 20:54:10 +0000

    connor v1.2.1

--- Formula/connor.rb
+++ Formula/connor.rb
@@ -1,10 +1,18 @@
+# Generated with JReleaser 0.10.0 at 2022-01-06T20:53:59.485454061Z
 class Connor < Formula
   desc "A commandline tool for resetting Kafka Connect source connector offsets."
   homepage "https://github.com/helpermethod/connor"
-  url "https://github.com/helpermethod/connor/releases/download/v1.2.0/connor-1.2.0-osx-x86_64.zip"
-  version "1.2.0"
-  sha256 "a3c4f5701bee1f56a25cf11f70d6449ff34691ad499ea8b5baaba12328acb719"
+  version "1.2.1"
   license "Apache-2"
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/helpermethod/connor/releases/download/v1.2.1/connor-1.2.1-linux-x86_64.zip"
+    sha256 "934efd81c805ced0f068ebf5628499cf506e7a77b4cddae9eb2bec1b3a9798c1"
+  end
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/helpermethod/connor/releases/download/v1.2.1/connor-1.2.1-osx-x86_64.zip"
+    sha256 "e18acd0bc4ef29d24702545be4e2b4fb5853c967852420b4b943ba6fbf0afd5a"
+  end
 
 
   def install
@@ -14,6 +22,6 @@ class Connor < Formula
 
   test do
     output = shell_output("#{bin}/connor --version")
-    assert_match "1.2.0", output
+    assert_match "1.2.1", output
   end
 end
