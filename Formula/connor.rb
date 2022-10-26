644266e6385dc38974b232cbe87a15158c9793c0
jreleaserbot <jreleaser@kordamp.org>
2022-10-26 09:15:29 +0000

    connor v1.4.1

--- Formula/connor.rb
+++ Formula/connor.rb
@@ -1,27 +1,20 @@
-# Generated with JReleaser 0.10.0 at 2022-01-06T20:53:59.485454061Z
+# Generated with JReleaser 1.2.0 at 2022-10-26T09:15:12.919251778Z
 class Connor < Formula
   desc "A commandline tool for resetting Kafka Connect source connector offsets."
   homepage "https://github.com/helpermethod/connor"
-  version "1.2.1"
+  url "https://github.com/helpermethod/connor/releases/download/v1.4.1/connor-1.4.1-osx-x86_64.zip"
+  version "1.4.1"
+  sha256 "a8ba17e7f19a00771e01839ea1cafb21a29e6d13ad2ffe0901bff8fa146b9fd5"
   license "Apache-2"
-
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/helpermethod/connor/releases/download/v1.2.1/connor-1.2.1-linux-x86_64.zip"
-    sha256 "934efd81c805ced0f068ebf5628499cf506e7a77b4cddae9eb2bec1b3a9798c1"
-  end
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/helpermethod/connor/releases/download/v1.2.1/connor-1.2.1-osx-x86_64.zip"
-    sha256 "e18acd0bc4ef29d24702545be4e2b4fb5853c967852420b4b943ba6fbf0afd5a"
-  end
 
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/connor"
+    bin.install_symlink "#{libexec}/bin/connor" => "connor"
   end
 
   test do
     output = shell_output("#{bin}/connor --version")
-    assert_match "1.2.1", output
+    assert_match "1.4.1", output
   end
 end
